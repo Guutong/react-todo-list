99ab08503f6ce7441e6d792a619da2152bc93234
Pornmongkon Pongsai <pornmongkonp@gmail.com>
2019-07-02 15:38:27 +0700

    🗑remove a todo

--- src/App.js
+++ src/App.js
@@ -19,16 +19,20 @@ function App() {
   ]);
 
   function handleKeyDown(e, i) {
-    if (e.key === 'Enter') {
+    if (e.key === "Enter") {
       createTodoAtIndex(e, i);
     }
+    if (e.key === 'Backspace' && todos[i].content === '') {
+      e.preventDefault();
+      return removeTodoAtIndex(i);
+    }
   }
-  
+
   function createTodoAtIndex(e, i) {
     const newTodos = [...todos];
     newTodos.splice(i + 1, 0, {
-      content: '',
-      isCompleted: false,
+      content: "",
+      isCompleted: false
     });
     setTodos(newTodos);
     setTimeout(() => {
@@ -40,6 +44,16 @@ function App() {
     const newTodos = [...todos];
     newTodos[i].content = e.target.value;
     setTodos(newTodos);
+  }
+
+  function removeTodoAtIndex(i) {
+    if (i === 0 && todos.length === 1) return;
+    setTodos(todos =>
+      todos.slice(0, i).concat(todos.slice(i + 1, todos.length))
+    );
+    setTimeout(() => {
+      document.forms[0].elements[i - 1].focus();
+    }, 0);
   }
   return (
     <div className="App">
@@ -54,7 +68,8 @@ function App() {
                   type="text"
                   value={todo.content}
                   onChange={e => updateTodoAtIndex(e, i)}
-                  onKeyDown={e => handleKeyDown(e, i)} />
+                  onKeyDown={e => handleKeyDown(e, i)}
+                />
               </div>
             ))}
           </ul>
