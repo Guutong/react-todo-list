fbbd4625511594da248f9b2032b27a3eb94d33ee
Pornmongkon Pongsai <pornmongkonp@gmail.com>
2019-07-02 15:22:23 +0700

    👆form todo list

--- src/App.js
+++ src/App.js
@@ -7,17 +7,14 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <form className="todo-list">
+          <ul>
+            <div className="todo">
+              <div className="checkbox" />
+              <input type="text" value="Todo one" />
+            </div>
+          </ul>
+        </form>
       </header>
     </div>
   );
