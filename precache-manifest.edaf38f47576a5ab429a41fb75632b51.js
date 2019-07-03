7ff2fa5686df5809ff3c32756cd8347982ea8b66
Pornmongkon Pongsai <pornmongkonp@gmail.com>
2019-07-03 11:07:49 +0700

    Updates

---
+++ precache-manifest.edaf38f47576a5ab429a41fb75632b51.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ceba066aafb1dbd0eef6a0764bb2191",
+    "url": "/react-todo-list/index.html"
+  },
+  {
+    "revision": "6ab5dfb0d22f02d5232f",
+    "url": "/react-todo-list/static/css/main.74f25dd8.chunk.css"
+  },
+  {
+    "revision": "1ff125cfb5784ede2a80",
+    "url": "/react-todo-list/static/js/2.a784ff31.chunk.js"
+  },
+  {
+    "revision": "6ab5dfb0d22f02d5232f",
+    "url": "/react-todo-list/static/js/main.44d9ec97.chunk.js"
+  },
+  {
+    "revision": "89737d6b5129f89a8a0b",
+    "url": "/react-todo-list/static/js/runtime~main.eb2f80b6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-todo-list/static/media/logo.5d5d9eef.svg"
+  }
+]);
